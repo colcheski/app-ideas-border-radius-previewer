b55bb2bcbe422145a899f618e4638da930536340
Chris Olcheski <24864308+colcheski@users.noreply.github.com>
2024-02-07 21:00:30 -0500

    put together box changing implementation with error validation

--- index.js
+++ index.js
@@ -1,17 +1,67 @@
 const topLeftInput = document.getElementById('topLeft');
+const topLeftError = document.getElementById('topLeftError');
 const topRightInput = document.getElementById('topRight');
+const topRightError = document.getElementById('topRightError');
 const bottomLeftInput = document.getElementById('bottomLeft');
+const bottomLeftError = document.getElementById('bottomLeftError');
 const bottomRightInput = document.getElementById('bottomRight');
+const bottomRightError = document.getElementById('bottomRightError');
+const inputs = [
+  {
+    inputElement: topLeftInput, 
+    errorTextElement: topLeftError
+  }, 
+  {
+    inputElement: topRightInput, 
+    errorTextElement: topRightError
+  }, 
+  {
+    inputElement: bottomLeftInput, 
+    errorTextElement: bottomLeftError
+  }, 
+  {
+    inputElement: bottomRightInput,
+    errorTextElement: bottomRightError
+  }
+];
 const box = document.getElementById('cornersBox');
 
-// topLeftInput.addEventListener('input', () => {
-//   console.log(topLeftInput.value);
-// });
+function validateInput(input, errorElement) {
+  if (isNaN(input.value)) {
+    input.classList.add('is-danger');
+    errorElement.classList.remove('is-hidden')
+  } else {
+    input.classList.remove('is-danger');
+    errorElement.classList.add('is-hidden')
+  }
+}
 
-console.log(getComputedStyle(box).borderTopLeftRadius);
+function updateBorderRadius(borderRadiusName, value) {
+  const style = box.style;
+  const pixelValue = value + 'px';
+  switch (borderRadiusName) {
+    case 'top-left':
+      style.borderTopLeftRadius = pixelValue;
+      break;
+    case 'top-right':
+      style.borderTopRightRadius = pixelValue;
+      break;
+    case 'bottom-left':
+      style.borderBottomLeftRadius = pixelValue;
+      break;
+    case 'bottom-right':
+      style.borderBottomRightRadius = pixelValue;
+      break;
+    default:
+      break;
+  }
+  
+}
 
-topLeftInput.value = parseFloat(getComputedStyle(box).borderTopLeftRadius);
-topRightInput.value = parseFloat(getComputedStyle(box).borderTopRightRadius);
-bottomLeftInput.value = parseFloat(getComputedStyle(box).borderBottomLeftRadius);
-bottomRightInput.value = parseFloat(getComputedStyle(box).borderBottomRightRadius);
+for (const input of inputs) {
+  input.inputElement.addEventListener('input', (event) => {
+    validateInput(input.inputElement, input.errorTextElement);
+    updateBorderRadius(event.target.name, input.inputElement.value);
+  })
+}
 
